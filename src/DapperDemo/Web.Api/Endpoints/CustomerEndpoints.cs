bf6d0562c1606f44dba519c93093f53a408dabba
nkastratovic <nkastratovic@gmail.com>
2024-03-19 11:22:29 +0100

    Creating the GET endpoint to fetch a single customer

--- src/DapperDemo/Web.Api/Endpoints/CustomerEndpoints.cs
+++ src/DapperDemo/Web.Api/Endpoints/CustomerEndpoints.cs
@@ -15,6 +15,14 @@ namespace Web.Api.Endpoints
                 var customers = await connection.QueryAsync<Customer>(sql);
                 return Results.Ok(customers);
             });
+
+            builder.MapGet("customers/id", async (int id, SqlConnectionFactory sqlConnectionFactory) =>
+            {
+                using var connection = sqlConnectionFactory.Create();
+                const string sql = "SELECT * FROM Customer WHERE Id = @CustomerId";
+                var customer = await connection.QuerySingleOrDefaultAsync<Customer>(sql, new { CustomerId = id });
+                return customer is not null ? Results.Ok(customer) : Results.NotFound();
+            });
         }
     }
 }
