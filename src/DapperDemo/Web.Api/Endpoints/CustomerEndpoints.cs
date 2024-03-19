f29ea41cdcf510bc77f67011029600eefb2c268b
nkastratovic <nkastratovic@gmail.com>
2024-03-19 22:03:09 +0100

    Add POST, PUT and, DELETE endpoint.

--- src/DapperDemo/Web.Api/Endpoints/CustomerEndpoints.cs
+++ src/DapperDemo/Web.Api/Endpoints/CustomerEndpoints.cs
@@ -23,6 +23,34 @@ namespace Web.Api.Endpoints
                 var customer = await connection.QuerySingleOrDefaultAsync<Customer>(sql, new { CustomerId = id });
                 return customer is not null ? Results.Ok(customer) : Results.NotFound();
             });
+
+            builder.MapPost("customers", async (Customer customer, SqlConnectionFactory sqlConnectionFactory) =>
+            {
+                using var connection = sqlConnectionFactory.Create();
+                const string sql = "INSERT INTO Customer (Id, FirstName, LastName, Email, DateOfBirth) VALUES (@Id, @FirstName, @LastName, @Email, @DateOfBirth)";
+                await connection.ExecuteAsync(sql, customer);
+                return Results.Ok();
+            });
+
+            builder.MapPut("customers/id", async (int id, Customer customer, SqlConnectionFactory sqlConnectionFactory) =>
+            {
+                using var connection = sqlConnectionFactory.Create();
+                const string sql = """
+                UPDATE Customer 
+                SET Id = @Id, FirstName = @FirstName, LastName = @LastName, Email = @Email, DateOfBirth = @DateOfBirth WHERE Id = @Id
+                """;
+                await connection.ExecuteAsync(sql, customer);
+                return Results.NoContent();
+            });
+
+
+            builder.MapDelete("customers/id", async (int id, SqlConnectionFactory sqlConnectionFactory) =>
+            {
+                using var connection = sqlConnectionFactory.Create();
+                const string sql = "DELETE from Customer WHERE Id = @CustomerId";
+                await connection.ExecuteAsync(sql, new { CustomerId = id });
+                return Results.NoContent();
+            });
         }
     }
 }
